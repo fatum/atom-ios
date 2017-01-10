8dc978dfdb6375d6404e119cc0f23456b2f53b9f
g8y3e <valentine.pavchuk@ironsrc.com>
2017-01-10 11:48:31 +0200

    fixed AtomSDK

--- atom-sdk/AtomSDK/AtomSDK/IronSourceAtom.swift
+++ atom-sdk/AtomSDK/AtomSDK/IronSourceAtom.swift
@@ -15,7 +15,7 @@ public typealias AtomCallback = (Response) -> Void
 /// Atom simple SDK
 public class IronSourceAtom {
     let TAG = "IronSourceAtom"
-    let API_VERSION_ = "V1.0.1"
+    let API_VERSION_ = "V1.1.0"
     
     var endpoint_ = "http://track.atom-data.io/"
     var authKey_ = ""
